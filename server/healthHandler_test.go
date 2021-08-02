f5c2a78fc924a495666637e435df19a6b7292147
Matthew Edge <medge@medgelabs.io>
2021-08-02 11:29:07 -0400

    Version check test

--- server/healthHandler_test.go
+++ server/healthHandler_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func TestHealthCheckHandler(t *testing.T) {
-
 	srv := New("1.0.0")
 
 	req, err := http.NewRequest("GET", "/health", nil)
@@ -25,6 +24,29 @@ func TestHealthCheckHandler(t *testing.T) {
 	}
 
 	expected := `{"status":"ok"}`
+	body := strings.TrimSpace(rr.Body.String())
+	if body != expected {
+		t.Errorf("unexpected body: Expected: %v | Got: %v", expected, body)
+	}
+}
+
+func TestVersionCheck(t *testing.T) {
+	srv := New("1.0.0")
+
+	req, err := http.NewRequest("GET", "/version", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(srv.versionCheck())
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("HTTP Error: Expected: %v | Got: %v", http.StatusOK, status)
+	}
+
+	expected := `{"version":"1.0.0"}`
 	body := strings.TrimSpace(rr.Body.String())
 	if body != expected {
 		t.Errorf("unexpected body: Expected: %v | Got: %v", expected, body)
