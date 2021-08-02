99ffe9063e31e2adb927c2eb01c9d0cf5cc51b97
Matthew Edge <medge@medgelabs.io>
2021-08-02 11:27:33 -0400

    Health check test

---
+++ server/healthHandler_test.go
@@ -0,0 +1,32 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestHealthCheckHandler(t *testing.T) {
+
+	srv := New("1.0.0")
+
+	req, err := http.NewRequest("GET", "/health", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(srv.healthCheck())
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("HTTP Error: Expected: %v | Got: %v", http.StatusOK, status)
+	}
+
+	expected := `{"status":"ok"}`
+	body := strings.TrimSpace(rr.Body.String())
+	if body != expected {
+		t.Errorf("unexpected body: Expected: %v | Got: %v", expected, body)
+	}
+}
